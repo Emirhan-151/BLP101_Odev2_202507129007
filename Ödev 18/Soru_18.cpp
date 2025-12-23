9c5dab899e330b8d6a5ab17297dead4bff73b342
Emirhan <83810259+Emirhan-151@users.noreply.github.com>
2025-12-24 00:28:31 +0300

    Refactor variable names for clarity in Soru_18.cpp

--- Ödev 18/Soru_18.cpp
+++ Ödev 18/Soru_18.cpp
@@ -2,19 +2,20 @@
 using namespace std;
 
 int main() {
-    int a;
-    int b;
+    int baslangıc;
+    int bitits;
     int sayi;
     cout<<"Baslangic degerini giriniz"<<endl<<"= "; //ekrana yazı yazdırır.
-    cin >> a;                                       //kullanıcıdan 1 sayı alır.
-    cout<<"Baslangic degerini giriniz"<<endl<<"= "; //ekrana yazı yazdırır.
-    cin >> b;                                       //kullanıcıdan 1 sayı alır.
-    for(a; a<b; a++){        //verilen başlangıç ve bitiş verilerine göre döngü oluşturur.
-        if(a%5 == 0){        //sayı 5 e kalansız bölünüyor ise...
-            cout<< a << " "; //ekrana kalansız bölünen sayıyı yazdırır.
+    cin >> baslangıc;                                       //kullanıcıdan 1 sayı alır.
+    cout<<"Bitis degerini giriniz"<<endl<<"= "; //ekrana yazı yazdırır.
+    cin >> bitis;                                       //kullanıcıdan 1 sayı alır.
+    for(baslangıc; baslangıc<bitis; baslangıc++){        //verilen başlangıç ve bitiş verilerine göre döngü oluşturur.
+        if(baslangıc%5 == 0){        //sayı 5 e kalansız bölünüyor ise...
+            cout<< baslangıc << " "; //ekrana kalansız bölünen sayıyı yazdırır.
         }
 
 
     }
 return(0);
+
 }
