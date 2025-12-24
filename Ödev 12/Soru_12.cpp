4812a7b0a8032c71676c47979e98b05f2f227045
Emirhan Çakır <emirhancakir2005@gmail.com>
2025-12-24 23:06:16 +0300

    : i i
    :qw
    :wq

--- Ödev 12/Soru_12.cpp
+++ Ödev 12/Soru_12.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -15,22 +14,5 @@ int main(){
     }
     cout << "Toplam = " << toplam <<endl;
     return(0);
-=======
-#include <iostream>
-using namespace std;
 
-int main(){
-    int n;
-    cout <<"toplamlarini istediginiz sayi = ";
-    cin >> n;
-    int toplam=0;
-
-    for(int i=0; i<=n; i++ ){   //kullanıcının girdiği değere gelene kadar i değerini 1 arttıran döngü.
-    
-        toplam=toplam+i;
-        
-    }
-    cout << "Toplam = " << toplam <<endl;
-    return(0);
->>>>>>> 44c79c3 (Düzeltmeler)
 }
--- Ödev 28/Soru_28.cpp
+++ Ödev 28/Soru_28.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <iostream>
 using namespace std;
 
@@ -25,32 +25,4 @@ int main() {
     }
 
     return 0;
-=======
-#include <iostream>
-using namespace std;
-
-int main() {
-    int sayi;
-    int yedek;
-    int ters = 0;
-    cout << "Bir sayi giriniz: ";
-    cin >> sayi;
-    yedek = sayi;
-    int gecici = sayi;
-    
-    while (gecici > 0) {
-        int sonBasamak = gecici % 10;
-        ters = (ters * 10) + sonBasamak;
-        gecici = gecici / 10;
-    }
-
-    
-    if (ters == yedek) {
-        cout << yedek << " bir Palindrom sayidir.";
-    } else {
-        cout << yedek << " bir Palindrom sayi degildir.";
-    }
-
-    return 0;
->>>>>>> 44c79c3 (Düzeltmeler)
 }
