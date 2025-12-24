e1a711b5ff7cddb8198a4405c89b2ccee7d0f590
Emirhan Çakır <emirhancakir2005@gmail.com>
2025-12-24 23:13:26 +0300

    Düzeltmeler2

--- Ödev 18/Soru_18.cpp
+++ Ödev 18/Soru_18.cpp
@@ -7,7 +7,11 @@ int main() {
     int sayi;
     cout<<"Baslangic degerini giriniz"<<endl<<"= "; //ekrana yazı yazdırır.
     cin >> baslangıc;                                       //kullanıcıdan 1 sayı alır.
+<<<<<<< HEAD
     cout<<"Bitis degerini giriniz"<<endl<<"= "; //ekrana yazı yazdırır.
+=======
+    cout<<"Baslangic degerini giriniz"<<endl<<"= "; //ekrana yazı yazdırır.
+>>>>>>> 9e89468 (Düzenleme yapildi)
     cin >> bitis;                                       //kullanıcıdan 1 sayı alır.
     for(baslangıc; baslangıc<bitis; baslangıc++){        //verilen başlangıç ve bitiş verilerine göre döngü oluşturur.
         if(baslangıc%5 == 0){        //sayı 5 e kalansız bölünüyor ise...
