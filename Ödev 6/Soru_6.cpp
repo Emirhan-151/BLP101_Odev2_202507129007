be45ccd9fddfaa5da0bc8dc32f2344314e34b451
Emirhan Çakır <emirhancakir2005@gmail.com>
2025-12-24 23:05:14 +0300

    Düzeltmeler2

--- Ödev 6/Soru_6.cpp
+++ Ödev 6/Soru_6.cpp
@@ -1,54 +1,26 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
 int main(){
-    int a;
-    int b;
+    int urun;
+    int indirim;
     cout <<"Urun fiyatini giriniz = "; //consol'a "Ürün fiyatını giriniz " yazdırır.
-    cin >> a ; //kullanıcıdan bir fiyat alır.
-    
-    if (a>=100 || 200<a) //girilen fiyat 100 tl ile 200 tl arasında ise 
-    {
-        b=(a*10)/100;   //girilen fiyata %10 indirim uygulanır.
-        cout<<"Odenecek turar : "<< a-b <<" TL"<<endl;  //ödenecek tutar ekrana yazdırılır.
-    }
-    else if (a >= 200) //girilen fiyat 200 tl üzerinde ise 
-    {
-        b=(a*20)/100; //girilen fiyata %20 indirim uygulanır.
-        cout<<"Odenecek turar : "<< a-b <<" TL"<<endl; //ödenecek tutar ekrana yazdırılır.
-    }
-    else //şartlar sağlanmıyo ise 
-    {
-        cout<<"Odenecek turar : "<< a <<" TL"<<endl; // ödenecek tutar ekrana yazdırılır.
-    }
-    
-    return(0);
-=======
-#include <iostream>
-using namespace std;
+    cin >> urun ; //kullanıcıdan bir fiyat alır.
 
-int main(){
-    int a;
-    int b;
-    cout <<"Urun fiyatini giriniz = "; //consol'a "Ürün fiyatını giriniz " yazdırır.
-    cin >> a ; //kullanıcıdan bir fiyat alır.
-    
-    if (a>=100 || 200<a) //girilen fiyat 100 tl ile 200 tl arasında ise 
+    if (urun>=100 && urun<200) //girilen fiyat 100 tl ile 200 tl arasında ise 
     {
-        b=(a*10)/100;   //girilen fiyata %10 indirim uygulanır.
-        cout<<"Odenecek turar : "<< a-b <<" TL"<<endl;  //ödenecek tutar ekrana yazdırılır.
+        indirim=(urun*10)/100;   //girilen fiyata %10 indirim uygulanır.
+        cout<<"Odenecek turar : "<< urun-indirim <<" TL"<<endl;  //ödenecek tutar ekrana yazdırılır.
     }
-    else if (a >= 200) //girilen fiyat 200 tl üzerinde ise 
+    else if (urun >= 200) //girilen fiyat 200 tl üzerinde ise 
     {
-        b=(a*20)/100; //girilen fiyata %20 indirim uygulanır.
-        cout<<"Odenecek turar : "<< a-b <<" TL"<<endl; //ödenecek tutar ekrana yazdırılır.
+        indirim=(urun*20)/100; //girilen fiyata %20 indirim uygulanır.
+        cout<<"Odenecek turar : "<< urun-indirim <<" TL"<<endl; //ödenecek tutar ekrana yazdırılır.
     }
     else //şartlar sağlanmıyo ise 
     {
-        cout<<"Odenecek turar : "<< a <<" TL"<<endl; // ödenecek tutar ekrana yazdırılır.
+        cout<<"Odenecek turar : "<< urun <<" TL"<<endl; // ödenecek tutar ekrana yazdırılır.
     }
     
     return(0);
->>>>>>> 44c79c3 (Düzeltmeler)
 }
