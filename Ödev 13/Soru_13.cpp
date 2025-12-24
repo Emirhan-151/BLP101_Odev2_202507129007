f97a118510fd3af362b8922595c11b2c4bcde172
Emirhan Çakır <emirhancakir2005@gmail.com>
2025-12-24 22:03:07 +0300

    Düzeltme

--- Ödev 13/Soru_13.cpp
+++ Ödev 13/Soru_13.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -19,26 +18,4 @@ int main(){
     }
     cout << "Faktoriyel = " << fakt <<endl;
     return(0);
-=======
-#include <iostream>
-using namespace std;
-
-int main(){
-    int girdi;
-    int fakt=1;
-    
-    do {
-        cout <<"Faktoriyel'ini istediginiz sayi = ";
-        cin >> girdi;
-        if (girdi<0){
-            cout<<"Negatif sayi girilemez!"<<endl;
-        }      
-    }
-     while (girdi < 0);
-        for (int i = 1; i <= girdi; i++) {
-        fakt = fakt * i;
-    }
-    cout << "Faktoriyel = " << fakt <<endl;
-    return(0);
->>>>>>> 44c79c3 (Düzeltmeler)
 }
